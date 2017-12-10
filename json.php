f22ea06ec1a46c8413883426db134e9b4dd9d352
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-10 23:42:51 +0200

    s

--- json.php
+++ json.php
@@ -4,7 +4,12 @@ if(isv("message")){
 $arrayName = array('AMsg' =>Num("message"),"TMsg"=>Num("message","where send='0'"),"SMsg"=>Num("message","where send='1'"));
 echo json_encode($arrayName);
 }else{
- $arrayName = array('Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
+  if(1 == 2){
+ $arrayName = array('error' =>false,'Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
  echo json_encode($arrayName);
+}else{
+  $arrayName = array('error' =>true,"msg"=>"no message !!");
+  echo json_encode($arrayName);
+}
 }
  ?>
