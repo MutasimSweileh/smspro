7b98e9155a36babd6938272f7e62e2ccf034b3b4
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-10 23:12:33 +0200

    s

--- json.php
+++ json.php
@@ -1,7 +1,7 @@
 <?php
 include "inc.php";
 if(isv("message")){
-$arrayName = array('AMsg' =>Num("message"),"NMsg"=>Num("message","where send='0'"));
+$arrayName = array('AMsg' =>Num("message"),"TMsg"=>Num("message","where send='0'"),"SMsg"=>Num("message","where send='1'"));
 echo json_encode($arrayName);
 }else{
  $arrayName = array('Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
