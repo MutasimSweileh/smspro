f6787d00cb49e5ee860a184c26cb911db65d193b
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-10 21:58:45 +0200

    s

--- json.php
+++ json.php
@@ -1,4 +1,5 @@
 <?php
-echo $arrayName = array('Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
+ $arrayName = array('Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
 
+echo json_encode($arrayName);
  ?>
