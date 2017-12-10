1d9c1bd3eb7d9fff4e30a3357b108d6bfa75e0d6
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-11 00:02:33 +0200

    s

--- json.php
+++ json.php
@@ -1,15 +1,26 @@
 <?php
 include "inc.php";
 if(isv("message")){
-$arrayName = array('AMsg' =>Num("message"),"TMsg"=>Num("message","where send='0'"),"SMsg"=>Num("message","where send='1'"));
-echo json_encode($arrayName);
+if(isv("id")){
+$sql =  UpDate("message","send",1,"where id=".isv("id"));
+if($sql){
+  echo json_encode(array('success' =>true));
 }else{
-  if(1 == 2){
- $arrayName = array('error' =>false,'Number' =>1119195568,"msg"=>"test Msg to send","date"=>time(),"cantry"=>"Egypt");
+    echo json_encode(array('success' =>false));
+}
+}else {
+  $arrayName = array('AMsg' =>Num("message"),"TMsg"=>Num("message","where send='0'"),"SMsg"=>Num("message","where send='1'"));
+  echo json_encode($arrayName);
+}
+}else{
+$S = Sel("message","where send='0'");
+if($S){
+ $arrayName = array('error' =>false,'id' =>$S->id,'Number' =>$S->number,"msg"=>$S->message,"date"=>$S->date,"cantry"=>$S->cantry);
  echo json_encode($arrayName);
 }else{
   $arrayName = array('error' =>true,"msg"=>"no message !!");
   echo json_encode($arrayName);
 }
+
 }
  ?>
