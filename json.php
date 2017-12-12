5d1cab396ca83d04e81819fa9e4351e1bcc09439
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 07:50:31 +0200

    d

--- json.php
+++ json.php
@@ -1,6 +1,15 @@
 <?php
 include "inc.php";
-if(isv("message")){
+if(isv("set")){
+$convert_to_array = explode(',',isv("set"));
+$convert_to_array2 = explode(',',isv("val"));
+for($i=0; $i < count($convert_to_array ); $i++){
+    $key_value = explode(',', $convert_to_array [$i]);
+    $key_value2 = explode(',', $convert_to_array2[$i]);
+    $end_array[$key_value [0]] = $key_value2 [0];
+}
+die(print_r($end_array));
+}else if(isv("message")){
 if(isv("id")){
 $sql =  UpDate("message","send",1,"where id=".isv("id"));
 if($sql){
