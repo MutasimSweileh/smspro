bc874912ca6bd771b155bb6060600350c458c5c9
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2017-12-12 21:07:31 +0200

    1

--- json.php
+++ json.php
@@ -8,18 +8,23 @@ for($i=0; $i < count($convert_to_array ); $i++){
     $key_value2 = explode(',', $convert_to_array2[$i]);
     $end_array[$key_value [0]] = $key_value2 [0];
 }
+$msg = "خطأ فى قاعدة البيانات ";
+$code = "sql_error";
 if(Num(isv("table"),"where $convert_to_array[0]='".$convert_to_array2[0]."'")){
 $Sql = false;
 if(isv("up")){
 $Sql =  UpDate(isv("table"),$end_array,null,"where $convert_to_array[0]='".$convert_to_array2[0]."'");
+}else{
+  $msg = "البيانات مسجله  بالفعل ";
+  $code = "sql_agin";
 }
 }else{
 $Sql =  SqlIn(isv("table"),$end_array);
 }
 if($Sql){
-  echo json_encode(array('success' =>true));
+  echo json_encode(array('success' =>true,"code"=>$code,"msg"=>"تم الحفظ بنجاح "));
 }else{
-    echo json_encode(array('success' =>false));
+    echo json_encode(array('success' =>false,"code"=>$code,"msg"=>$msg));
 }
 }else if(isv("message")){
 if(isv("id")){
